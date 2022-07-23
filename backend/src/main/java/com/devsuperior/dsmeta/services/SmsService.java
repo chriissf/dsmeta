c7a8c38b63f139742b23a0ca277eaeae4cec1310
perkles <christian.fdacr@gmail.com>
2022-07-23 14:28:25 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dsmeta.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repository.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -22,15 +25,25 @@ public class SmsService {
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
 
-	public void sendSms() {
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+
+		Sale sale = saleRepository.findById(saleId).get();
+
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		String msg = "O Vendedor " + sale.getSellerName() + " Foi Destaque em " + date + "  com um total de R$ "
+				+ String.format("%2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom); 
 
-		Message message = Message.creator(to, from, "Christian Lindo").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}
+
 }
